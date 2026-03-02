b7136c0ff20d62bbcae77627d8b75813f1034541
DeltaStudent-2002 <maheshdhondge26@gmail.ocm>
2026-03-02 09:04:41 +0530

    added

--- Backend/app.js
+++ Backend/app.js
@@ -5,15 +5,17 @@ const cors = require('cors');
 const app = express();
 const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db');
-const userRoutes = require('./routes/user.routes');
-const captainRoutes = require('./routes/captain.routes');
-const mapsRoutes = require('./routes/maps.routes');
-const rideRoutes = require('./routes/ride.routes');
-const paymentRoutes = require('./routes/payment.routes');
 
 connectToDb();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://uber-ryde-bymahesh.vercel.app"
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -22,10 +24,6 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.use(cors({
-  origin: 'https://uber-ryde-bymahesh.vercel.app/',
-  credentials: true
-}))
 app.use('/users', userRoutes);
 app.use('/captains', captainRoutes);
 app.use('/maps', mapsRoutes);
