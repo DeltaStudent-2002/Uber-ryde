f2add72736360b6f55c1911d7f6f70aedf4e382d
Mahesh Dhondge <153748186+DeltaStudent-2002@users.noreply.github.com>
2026-03-08 11:04:49 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -19,7 +19,7 @@ connectToDb();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-   "https://uber-ryde-10wy7dxfm-deltastudent-2002s-projects.vercel.app"
+   "https://uber-ryde.vercel.app/"
   ],
   credentials: true
 }));
